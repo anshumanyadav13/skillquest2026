2f4240fcc73154e6a9d41cdea878a015733a5fa2
anshumanyadav13 <anshumanyadav6119@gmail.com>
2026-07-15 16:17:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,26 +18,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
-import {
-    getFirestore,
-    doc,
-    getDoc
-} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyAn2zYgbD3LqawWMGVmGLWEwRs4ZNu-iiI",
-    authDomain: "skillquestrj2026.firebaseapp.com",
-    projectId: "skillquestrj2026",
-    storageBucket: "skillquestrj2026.firebasestorage.app",
-    messagingSenderId: "180834121471",
-    appId: "1:180834121471:web:c5fa92f0a5c34d8319feca",
-    measurementId: "G-TWML8ZN1Q4"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
 window.startQuiz = async function () {
 
     const name = document.getElementById("name").value.trim();
