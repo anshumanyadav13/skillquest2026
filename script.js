6b526cab0e358bd268c80cb462b184e81c4d0f38
anshumanyadav13 <anshumanyadav6119@gmail.com>
2026-07-16 16:23:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,6 +18,43 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-window.startQuiz = function () {
-    alert("Button is working");
+window.startQuiz = async function () {
+
+    const name = document.getElementById("name").value.trim();
+    const roll = document.getElementById("roll").value.trim();
+    const cls = document.getElementById("class").value;
+
+    if (name === "") {
+        alert("Please enter your name.");
+        return;
+    }
+
+    if (!/^[A-Za-z ]+$/.test(name)) {
+        alert("Name should contain only letters.");
+        return;
+    }
+
+    if (!/^\d{4}$/.test(roll)) {
+        alert("Roll Number must be exactly 4 digits.");
+        return;
+    }
+
+    try {
+    const studentRef = doc(db, "scores", roll);
+    const studentDoc = await getDoc(studentRef);
+
+    if (studentDoc.exists()) {
+        alert("❌ This Roll Number has already attempted the quiz.");
+        return;
+    }
+} catch (error) {
+    alert(error.message);
+    return;
 }
+
+    localStorage.setItem("name", name);
+    localStorage.setItem("roll", roll);
+    localStorage.setItem("class", cls);
+
+    window.location.href = "rules.html";
+};
